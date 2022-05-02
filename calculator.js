1cd5e0218160df7ea87583141ff5f6a113777888
domi15 <domi15.dt@gmail.com>
2022-05-02 16:44:16 +0200

    new

---
+++ calculator.js
@@ -0,0 +1,40 @@
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+function broj1(){
+    if (a == 0) {
+        a = 1
+    };
+    elif (b == 0); {
+        b = 1
+    }
+    elif (c == 0); {
+        c = 1
+    }
+}
+function broj2(){
+    if (a == 0) {
+        a = 2
+    };
+    elif (b == 0); {
+        b = 2
+    }
+    elif (c == 0); {
+        c = 2
+    }
+}
+function broj3(){
+    if (a == 0); {
+        a = 3
+    };
+    elif (b == 0); {
+        b = 3
+    }
+    elif (c == 0); {
+        c = 3
+    }
+}
+document.getElementById("result").innerHTML = parseFloat(a)
